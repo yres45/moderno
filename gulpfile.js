1b5de78b0729ce368d2f364157bf4cf9ed312715
yres45 <yres22@gmail.com>
2020-02-11 07:58:21 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let gulp = require('gulp'),
 
 
 gulp.task('sass',function(){
-  return  gulp.src('app/scss/style.scss')
+  return  gulp.src('app/scss/**/*.scss')
              .pipe(sass({outputStyle:' expanded '}))
               .pipe(rename({suffix : '.min'}))
               .pipe(autoprefixer({
@@ -66,7 +66,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'));
     gulp.watch('app/js/*.js', gulp.parallel('js'));     
 });
