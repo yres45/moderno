1dcab67b8a6fb66946ca6510fed6fabcd5e2757b
yres45 <yres22@gmail.com>
2020-02-17 13:55:31 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -15,6 +15,35 @@ $(function(){
 
 
     });
+
+
+    $(".js-range-slider").ionRangeSlider({
+       
+            type: "double",            
+            min: 0,
+            max: 1000,
+            from: 0,
+            to: 600,
+            prefix: "$"
+        
+    });
+
+$('.icon-th-list').on('click',function(){
+    $('.products__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+
+});
+
+
+
+$('.icon-th-large').on('click',function(){
+    $('.products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+
+});
+    
     var mixer = mixitup ('.products__inner-box');
 
     
--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,8 @@ gulp.task('style', function(){
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/rateyo/src/jquery.rateyo.css'    
+        'node_modules/rateyo/src/jquery.rateyo.css',
+        'node_modules/ion-rangeslider/css/ion.rangeSlider.css'    
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -38,7 +39,9 @@ gulp.task('script', function(){
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js',       
         'node_modules/mixitup/dist/mixitup.js',
-        'node_modules/rateyo/src/jquery.rateyo.js'
+        'node_modules/rateyo/src/jquery.rateyo.js',
+        'node_modules/ion-rangeslider/js/ion.rangeSlider.js'
+        
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
