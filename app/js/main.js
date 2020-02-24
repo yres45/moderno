9a43a39b033fa73a5a814a7b18fc3aebe2813f21
yres45 <yres22@gmail.com>
2020-02-24 13:40:45 +0300

    added settings page

--- app/js/main.js
+++ app/js/main.js
@@ -13,10 +13,10 @@ $(function(){
         slidesToScroll:4
     });
 
-    $('.product-one__tabs .tab').on('click', function(event) {
+    $('.product-one__tabs .tab, .settings__taabs .tab').on('click', function(event) {
         var id = $(this).attr('data-id');
-            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $('.product-one__tabs, .settings__taabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs, .settings__taabs .tabs').find('.tab').removeClass('active');
             $(this).addClass('active');
             $('#'+id).addClass('active-tab').fadeIn();
             return false;
